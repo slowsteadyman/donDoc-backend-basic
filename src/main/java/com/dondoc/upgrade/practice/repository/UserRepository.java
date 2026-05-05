ab78fbc2b613206bc61ca3229e38c40b65b752bf
slowsteadyman <117254015+slowsteadyman@users.noreply.github.com>
2026-05-05 22:43:46 +0900

    Feature/#8 (#9)
    * feat(controller): controller 3개로 분류 (user, farm, book)
    
    * feat(dto): controller와 동일하게 dto도 분리
    
    * feat(UserRepository) #8

---
+++ src/main/java/com/dondoc/upgrade/practice/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.dondoc.upgrade.practice.repository;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Map<String, Object>> findAll() {
+        return jdbcTemplate.queryForList("SELECT * FROM users");
+    }
+}
+
